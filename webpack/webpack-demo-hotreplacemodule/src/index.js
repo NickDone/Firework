45af5e03e056fdb86d65a191d4e3d94ed36566c6
lixd <651309599@qq.com>
2018-08-24 15:02:51 +0800

    add some learn proj

---
+++ webpack/webpack-demo-hotreplacemodule/src/index.js
@@ -0,0 +1,22 @@
+import _ from 'lodash';
+import printMe from './print.js';
+
+function component() {
+  var element = document.createElement('div');
+  // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+  element.innerHTML = _.join(['a Hello aha', 'webpack'], ' ');
+  var btn=document.createElement("button");
+  btn.innerHTML="Click me and see the console";
+  btn.onclick=printMe;
+  element.appendChild(btn);
+  return element;
+}
+
+document.body.appendChild(component());
+
+if (module.hot) {
+    module.hot.accept('./print.js', function() {
+      console.log('Accepting the updated printMe module!');
+      printMe();
+    })
+  }
---
+++ webpack/webpack-demo-hotreplacemodule/src/print.js
@@ -0,0 +1,3 @@
+export default function printMe() {
+    console.log('I am I get called from print.js!');
+  }
